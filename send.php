b48b09bdb89a7d21e9510730689b1c86291b7c6d
romanchik4261 <85174332+romanchik4261@users.noreply.github.com>
2022-04-14 16:48:07 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,12 +1,18 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 // Файлы phpmailer
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/Exception.php';
+
+$mail = new PHPMailer(true);
+$mail ->CharSet = 'UTF-8'; 
 
 // Переменные, которые отправляет пользователь
 $name = $_POST['name'];
 $email = $_POST['email'];
+$phone = $_POST['phone'];
 $style $_POST['style'];
 $message = $_POST['message'];
 $age = $_POST['age'];
@@ -15,34 +21,46 @@ $file = $_FILES['myfile'];
 // Формирование самого письма
 $title = "Заголовок письма";
 $body = "
-<h2>Новое письмо</h2>
+<h2>Новое письмо</h2><br>
 <b>Имя:</b> $name<br>
 <b>Почта:</b> $email<br><br>
+<b>Телефон:</b> $phone<br><br>
 <b>Стиль:</b> $style<br><br>
 <b>Сообщение:</b><br>$message
 <b>Возраст:</b><br>$age
 ";
+$theme = "[Заявка с формы]";
+
+// Получатель письма, если нужен ещё один, ввести отдельной строкой
+$mail->addAddress('romanchik4261@mail.ru');
+
+// Отправка сообщения
+$mail->Subject = $theme;
+$mail->Body = $body;
+// $mail->isHTML(true);
+
+$mail->send();
+
+
 
 // Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
-    $mail->isSMTP();   
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
-    $mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+// $mail = new PHPMailer\PHPMailer\PHPMailer();
+// try {
+//     $mail->isSMTP();   
+//     $mail->CharSet = "UTF-8";
+//     $mail->SMTPAuth   = true;
+//     $mail->SMTPDebug = 2;
+//     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
-    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
-    $mail->Username   = 'romanchik4261'; // Логин на почте
-    $mail->Password   = 'vB5DiedbpFSwVX1fZ5zw'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('mail@yandex.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
+    // $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    // $mail->Username   = 'romanchik4261'; // Логин на почте
+    // $mail->Password   = 'vB5DiedbpFSwVX1fZ5zw'; // Пароль на почте
+    // $mail->SMTPSecure = 'ssl';
+    // $mail->Port       = 465;
+    // $mail->setFrom('mail@yandex.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
 
-    // Получатель письма
-    $mail->addAddress('youremail@yandex.ru');  
-    $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+    
 
     // Прикрипление файлов к письму
 if (!empty($file['name'][0])) {
@@ -57,16 +75,13 @@ if (!empty($file['name'][0])) {
         }
     }   
 }
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
+    
 
 // Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
 else {$result = "error";}
 
-} catch (Exception $e) {
+catch (Exception $e) {
     $result = "error";
     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
